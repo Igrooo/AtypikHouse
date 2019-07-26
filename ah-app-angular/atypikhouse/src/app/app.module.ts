5e835542700a191002557e5d726658e3589d1a21
Igor <dev@ifaidy.infini.fr>
2019-07-26 14:17:57 +0200

    WPA Angular - API Server

--- ah-app-angular/atypikhouse/src/app/app.module.ts
+++ ah-app-angular/atypikhouse/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { HouseComponent } from './house/house.component';
 import { Routes, RouterModule } from "@angular/router";
 
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 const routes : Routes = [
   { path: '', component: ListComponent },
@@ -28,7 +29,7 @@ const routes : Routes = [
   ],
   imports: [
     RouterModule.forRoot(routes),
-    FormsModule,
+    FormsModule, HttpClientModule,
     BrowserModule, BrowserAnimationsModule,
     MatButtonModule, MatIconModule, MatInputModule, MatSelectModule, MatSliderModule,
     MatToolbarModule, MatCardModule, MatSlideToggleModule
--- ah-app-angular/atypikhouse/src/app/data.service.ts
+++ ah-app-angular/atypikhouse/src/app/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { House} from "./logic/House";
 import { PlaceLocation } from "./logic/PlaceLocation";
+import { HttpHeaders, HttpClient, HttpParams } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -8,19 +9,38 @@ import { PlaceLocation } from "./logic/PlaceLocation";
 
 export class DataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public endpoint = "http://localhost:3000"; //API Server URL
 
   getList(callback) {
     //TODO: real web service
+    /*
     const list = [
       new House("Tipi de Roger", "Chez Roger", new PlaceLocation("Lieu dit inconnu", "Plouguernével")),
       new House("Cabane suspendue", "Paradis", new PlaceLocation("Boulevard des amoureux", "Cité du Paradis")),
     ];
     callback(list);
+    */
+    this.http.get(`${this.endpoint}/houses`)
+      .subscribe(response => {
+      console.log(response);
+      callback(response);
+    });
+
   }
 
   save(house, callback) {
     //TODO: real web service
+    if (house._id) {
+      // It's an update
+      this.http.put(`${this.endpoint}/houses/${house._id}`, house)
+        .subscribe(response => {
+          callback(true);
+        });
+    } else {
+      // It's an insert
+    }
     callback(true);
   }
 }
