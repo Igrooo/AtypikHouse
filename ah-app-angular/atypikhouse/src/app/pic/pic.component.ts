e1eeb03c6d0e9aab4ed6b54c7db5bc85e7bfa05a
Igor <dev@ifaidy.infini.fr>
2019-08-26 14:37:59 +0200

    Logic Classes

---
+++ ah-app-angular/atypikhouse/src/app/pic/pic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pic',
+  templateUrl: './pic.component.html',
+  styleUrls: ['./pic.component.less']
+})
+export class PicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
