d526245c936edb9663b61726599b8c28f646c895
Igor <dev@ifaidy.infini.fr>
2020-01-30 18:48:49 +0100

    responsive css

--- atypikhouse/src/app/elems/elem-map/map.component.ts
+++ atypikhouse/src/app/elems/elem-map/map.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styles: ['agm-map { height: 600px; }']
+  styles: ['agm-map { height: 600px; }  @media screen and (max-width: 580px) { agm-map { height: 300px; } }']
 })
 export class MapComponent implements OnInit {
 
