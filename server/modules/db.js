0d4337a0c872f26011e23c93a5193aac5ac7eee6
AmineBahadi <49941680+AmineBahadi@users.noreply.github.com>
2020-02-20 14:08:56 +0100

    Update db.js

--- server/modules/db.js
+++ server/modules/db.js
@@ -1,7 +1,7 @@
 let mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host: '37.59.61.46/phpmyadmin',
+    host: 'localhost',
     user: 'amine',
     password: 'aminou@14',
     port: "3306",
