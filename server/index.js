642ca94d2ddf002b498be6f8b28b1469f47b141f
Igrooo <dev@ifaidy.infini.fr>
2020-02-22 21:42:06 +0100

    test

--- server/index.js
+++ server/index.js
@@ -1,28 +1,3 @@
-'use strict';
-
-var os = require('os');
-var ifaces = os.networkInterfaces();
-
-Object.keys(ifaces).forEach(function (ifname) {
-  var alias = 0;
-
-  ifaces[ifname].forEach(function (iface) {
-    if ('IPv4' !== iface.family || iface.internal !== false) {
-      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-      return;
-    }
-
-    if (alias >= 1) {
-      // this single interface has multiple ipv4 addresses
-      console.log(ifname + ':' + alias, iface.address);
-    } else {
-      // this interface has only one ipv4 adress
-      console.log(ifname, iface.address);
-    }
-    ++alias;
-  });
-});
-/*
 // IMPORT ROUTES
 import login   from './routes/login';
 import signup  from './routes/signup';
@@ -91,4 +66,3 @@ app.use(remove);
 
 // MIDDLEWARE D'AUTHENTIFICATION //////////////////////////
 //app.use(auth);
-*/
