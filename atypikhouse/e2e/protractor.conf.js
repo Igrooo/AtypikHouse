2958fac7d3f1972008fbdbccbe8df9729e5265f8
AmineBahadi <49941680+AmineBahadi@users.noreply.github.com>
2020-03-02 15:02:56 +0100

    Update protractor.conf.js

--- atypikhouse/e2e/protractor.conf.js
+++ atypikhouse/e2e/protractor.conf.js
@@ -16,7 +16,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'http://37.59.61.46:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
